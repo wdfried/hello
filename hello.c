bac86c1750ab7d710620f400e677055955730d03
Will Friedenberger <wfriedenberger2@huskers.unl.edu>
2021-08-25 20:25:33 +0000

    initial commit

---
+++ hello.c
@@ -0,0 +1,15 @@
+ /**
+ * Date: 8/25/2021
+ *
+ * A simple hello world program in C
+ *
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+    printf("Will Friedenberger\n");
+
+    return 0;
+}
